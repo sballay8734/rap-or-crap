1fcc94f0935dd6c994d4bbb72a33ec2f64d34c38
sballay8734 <48398020+sballay8734@users.noreply.github.com>
2024-02-10 16:58:27 -0500

    tsup config

--- packages/ui/tsup.config.ts
+++ packages/ui/tsup.config.ts
@@ -1,8 +1,18 @@
-import { defineConfig, type Options } from "tsup";
+import { defineConfig, type Options } from "tsup"
 
 export default defineConfig((options: Options) => ({
-  banner: {
-    js: "'use client'",
-  },
-  ...options,
-}));
+  // banner: {
+  //   js: "'use client'",
+  // },
+  treeshake: true,
+  splitting: true,
+  // TODO: Issue could be here
+  entry: ["./**/*.tsx"],
+  // format: ["esm"],
+  // dts: true,
+  minify: true,
+  clean: true,
+  // TODO: Or here because this flag is used in package.json
+  // external: ["react"],
+  ...options
+}))
