939b52e5e74fba930040923e08d33c4585370887
sballay8734 <48398020+sballay8734@users.noreply.github.com>
2024-02-11 07:47:49 -0500

    Add prettier auto-sort tailwind classes

--- apps/frontend/src/app/index.tsx
+++ apps/frontend/src/app/index.tsx
@@ -1,5 +1,5 @@
-import "../../styles/global.css"
-import { CounterButton, Link } from "@repo/ui"
+import "../../styles/global.css";
+import { CounterButton, Link } from "@repo/ui";
 
 // DEPLOYMENT ONLY WORKS IF YOU RUN SERVER LOCALLY
 // You'll probably have to deploy server on vercel somehow to
@@ -8,35 +8,35 @@ function App(): JSX.Element {
   const testObject = {
     artistName: "Bob Dillon",
     lyric: "Oh baby baby what are you doing and where did you go?",
-    youtubeUrl: "string"
-  }
+    youtubeUrl: "string",
+  };
 
   async function testServer() {
     const res = await fetch("http://localhost:5001/api/prompts/create-prompt", {
       method: "POST",
       headers: {
-        "Content-Type": "application/json"
+        "Content-Type": "application/json",
       },
-      body: JSON.stringify(testObject)
-    })
+      body: JSON.stringify(testObject),
+    });
 
-    if (!res.ok) return // throw error
+    if (!res.ok) return; // throw error
 
-    const data = await res.json()
+    const data = await res.json();
 
-    console.log(data)
+    console.log(data);
   }
 
   return (
     <div className="container">
       <button
-        className="text-green-700 font-bold p-6 m-4 border-2 border-red-400"
+        className="bg-sky-700 px-4 py-2 text-white hover:bg-sky-800 sm:px-8 sm:py-3"
         onClick={testServer}
       >
         Test Send Prompt
       </button>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
