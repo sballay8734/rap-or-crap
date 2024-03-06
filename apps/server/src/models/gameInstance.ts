a0334a1d923ad52f38f8ffa9a7ed8a0756b71e10
Shawn Ballay <shawnballay1@gmail.com>
2024-03-06 12:51:43 -0500

    NOTE

--- apps/server/src/models/gameInstance.ts
+++ apps/server/src/models/gameInstance.ts
@@ -7,6 +7,7 @@ interface IGameInstance {
 const GameInstanceSchema = new Schema({
   // Use the game instance to continue game rather than dealing with local storage.
   // you can have an "active game" field for each user. When they select new game, you clear this field and replace it with the new one.
+  // Delete the game instance when a new one is created
 });
 
 const Game = mongoose.model<IGameInstance>("game", GameInstanceSchema);
