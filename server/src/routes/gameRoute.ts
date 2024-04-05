71c8f3d53c6984cf06bc81d937e5ab76a5ddc24e
sballay8734 <48398020+sballay8734@users.noreply.github.com>
2024-04-04 21:21:54 -0400

    chore: Add main todos for friday work session

--- server/src/routes/gameRoute.ts
+++ server/src/routes/gameRoute.ts
@@ -12,6 +12,22 @@ import { authenticateUser } from "../middleware/authenticateUser"
 const router = express.Router()
 
 router.post("/initialize-game", authenticateUser, initializeGame)
+
+// TODO: **********************************************************************
+// TODO: **********************************************************************
+// TODO: **********************************************************************
+// TODO: **********************************************************************
+// TODO: **********************************************************************
+
+// TODO: Add console logs in active-game to see what route is being triggered on logout and causing "unauthorized" error response
+
+// TODO: Also, look into "fetchActiveGame" sometimes appearing inside modals to render and not being removed (hard to replicate but keep and eye on it)
+
+// TODO: **********************************************************************
+// TODO: **********************************************************************
+// TODO: **********************************************************************
+// TODO: **********************************************************************
+// TODO: **********************************************************************
 router.get("/active-game", authenticateUser, fetchActiveGame)
 router.delete("/delete-game", authenticateUser, deleteOldActiveGame)
 router.patch("/update-game", authenticateUser, updateGame)
