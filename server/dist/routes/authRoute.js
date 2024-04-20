a57c399b080d6f381b8dcc7cca57ed1e7d5f90f0
sballay8734 <48398020+sballay8734@users.noreply.github.com>
2024-04-20 10:17:46 -0400

    chore: Run build

--- server/dist/routes/authRoute.js
+++ server/dist/routes/authRoute.js
@@ -5,8 +5,10 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const authController_1 = require("../controllers/authController");
+const authenticateUser_1 = require("../middleware/authenticateUser");
 const router = express_1.default.Router();
 router.post("/signup", authController_1.signup);
+router.post("/signup-guest", authController_1.signupGuest);
 router.post("/signin", authController_1.signin);
-router.post("/signout", authController_1.signout);
+router.post("/signout", authenticateUser_1.authenticateUser, authController_1.signout);
 exports.default = router;
